5dce7530f338b823b475525cc89af81a237d79e1
Muraleekrishna Gopinathan <muraleekrishna.gopinathan@hitachirail.com>
2025-05-23 13:52:15 +0800

    Vectorize for loops

--- train.py
+++ train.py
@@ -5,23 +5,28 @@ import numpy as np
 ## Changes 
 # 1. rename unicode variables to readable ascii
 # 2. move `distance` to a method 
+# 3. Vectorize for loop
 
 def train(input_data, n_max_iterations, width, height):
     sigma_0 = max(width, height) / 2
     alpha_0 = 0.1
     weights = np.random.random((width, height, 3))
     time_const = n_max_iterations / np.log(sigma_0)
+    
+    x = np.arange(width)
+    y = np.arange(height)
+    x_coords, y_coords = np.meshgrid(x, y, indexing='ij')
     for t in range(n_max_iterations):
         sigma_t = sigma_0 * np.exp(-t/time_const)
         alpha_t = alpha_0 * np.exp(-t/time_const)
         for vt in input_data:
             bmu = np.argmin(np.sum((weights - vt) ** 2, axis=2))
             bmu_x, bmu_y = np.unravel_index(bmu, (width, height))
-            for x in range(width):
-                for y in range(height):
-                    di = distance(bmu_x, bmu_y, x, y)
-                    theta_t = np.exp(-(di ** 2) / (2*(sigma_t ** 2)))
-                    weights[x, y] += alpha_t * theta_t * (vt - weights[x, y])
+                    
+            di = distance(bmu_x, bmu_y, x_coords, y_coords)
+            theta_t = np.exp(-(di ** 2) / (2*(sigma_t ** 2)))
+            weights += alpha_t * theta_t[:, :, np.newaxis] * (vt - weights)
+
     return weights
 
 def distance(bmu_x, bmu_y, x, y):
@@ -43,6 +48,6 @@ if __name__ == '__main__':
     # Generate data
     image_data = train(input_data, 1000, 100, 100)
     end_time = timeit.default_timer()
-    print(f"Duration 2: {end_time} - {start_time}s")
+    print(f"Duration 2: {end_time - start_time}s")
 
     plt.imsave('1000.png', image_data)
